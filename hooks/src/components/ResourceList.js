e3824df6f567ed6200493c5376114289e055e709
florian lahitte <lahitte.florian@gmail.com>
2019-06-26 09:57:40 -0400

    Add hooks method

--- hooks/src/components/ResourceList.js
+++ hooks/src/components/ResourceList.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import useResources from './useResources';
 
-class ResourceList extends React.Component{
-    render(){
-        return (
-            <div>{this.props.resource}</div>
-        )
-    }
+const ResourceList = ({resource}) =>{
+    const resources = useResources(resource);
+    return (
+        <ul>{resources.map(resource =>
+            <li key={resource.id}>{resource.title}</li>
+        )}</ul>
+    )
+
 };
 
 export default ResourceList;
